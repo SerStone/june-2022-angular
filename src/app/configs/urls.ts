af2e519fdc97fbda3a74ee6c05a5cd3d9ad02960
SerStone <mishagoal752@gmail.com>
2022-11-02 17:13:26 +0200

    Angular1

---
+++ src/app/configs/urls.ts
@@ -0,0 +1,13 @@
+import {Comment} from "@angular/compiler";
+
+const baseURL = 'https://jsonplaceholder.typicode.com';
+
+const urls = {
+  users:`${baseURL}/users`,
+  posts:`${baseURL}/posts`,
+  comments:`${baseURL}/comments`
+}
+
+export {
+  urls
+}
